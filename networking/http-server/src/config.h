2b05b349cdb63dc57f4e140de808fd0f3690cc46
hbobenicio <hbobenicio@gmail.com>
2023-08-05 10:35:21 -0300

    config: organizing code with more static functions and cumulative error reporting

--- networking/http-server/src/config.h
+++ networking/http-server/src/config.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 #include "error.h"
 
@@ -13,13 +14,14 @@ struct config {
     const char* server_host;
     uint16_t server_port;
     uint16_t tcp_backlog;
+    size_t read_timeout_ms;
+    size_t write_timeout_ms;
+    size_t connection_timeout_ms;
 };
 
 /**
  * Initializes the configuration from environment variables values.
  *
  * @param c The configuration to be loaded, where data should be saved
- * @return On success, 0 is returned
  */
 enum return_code config_init_from_env(struct config* c);
-
